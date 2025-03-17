75f22777cff246f5f46f9df6d20ff4f7cf221b08
DESKTOP-MVJ1FGJ\yzw <1494993218@qq.com>
2025-03-18 00:03:28 +0800

    添加controller

--- routes/admin/settings.js
+++ routes/admin/settings.js
@@ -2,38 +2,16 @@ const express = require('express')
 const router = express.Router()
 const { Setting } = require('../../models')
 const { success, failure } = require('../../utils/response')
-
+const SettingController = require('../../controllers/setting.controller')
 
 /**
  * 查看设置详情
  * GET /admin/settings/1
  */
-router.get('/', async function (req, res) {
-	let id = req.params.id
-
-	try {
-		const data = await Setting.findOne()
-		success(res, data)
-	} catch (e) {
-		failure(res, e)
-	}
-}) 
+router.get('/:id', (req, res)=> SettingController.get(req, res))
 /**
  *	更新设置
  * PUT /admin/settings
  */
-router.put('/', async function (req, res) {
-	let body = {
-		name: req.body.name,
-		icp: req.body.content,
-		copyright: req.body.copyright,
-	}
-	try {
-		const article = await Setting.findByPk(1)
-		await article.update(body)
-		success(res, null)
-	} catch (e) {
-		failure(res, e)
-	}
-})
+router.put('/', (req, res)=> SettingController.put(req, res))
 module.exports = router
